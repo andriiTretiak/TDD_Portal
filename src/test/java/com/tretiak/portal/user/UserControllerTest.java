71cd7ce08982f94aaa3a9dbe43c75690bba2ec25
Andrey <CPdSCabellos@gmail.com>
2020-03-02 19:35:38 +0200

    Add User object and user controller

---
+++ src/test/java/com/tretiak/portal/user/UserControllerTest.java
@@ -0,0 +1,35 @@
+package com.tretiak.portal.user;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
+public class UserControllerTest {
+
+    private static final String API_1_0_USERS = "/api/1.0/users";
+
+    @Autowired
+    private TestRestTemplate testRestTemplate;
+
+    @Test
+    public void postUser_whenUserIsValid_receiveOk(){
+        User user = new User();
+        user.setUsername("test-user");
+        user.setDisplayName("test-display");
+        user.setPassword("P4ssword");
+
+        ResponseEntity<Object> response = testRestTemplate.postForEntity(API_1_0_USERS, user, Object.class);
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+    }
+}
