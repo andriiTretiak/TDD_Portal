7a2da3dbc5170c21750f568f3d033e1722f905e8
Andrey <CPdSCabellos@gmail.com>
2020-03-02 20:15:24 +0200

    (Back) Hashed user password before save to DB

--- src/main/java/com/tretiak/portal/user/UserService.java
+++ src/main/java/com/tretiak/portal/user/UserService.java
@@ -1,17 +1,21 @@
 package com.tretiak.portal.user;
 
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
 
     private final UserRepository userRepository;
+    private final BCryptPasswordEncoder passwordEncoder;
 
     public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
+        this.passwordEncoder = new BCryptPasswordEncoder();
     }
 
     User save(User user){
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
         return userRepository.save(user);
     }
 }
