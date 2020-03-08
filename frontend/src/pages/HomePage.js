f86378b8f025ddf914c4b527b67b84a92bd55f9a
Andrey <CPdSCabellos@gmail.com>
2020-03-08 12:08:35 +0200

    (Front) Add UserPage and HomePage

---
+++ frontend/src/pages/HomePage.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export class HomePage extends React.Component{
+    render() {
+        return (
+            <div data-testid="homepage">
+
+            </div>
+        )
+    }
+}
+
+export default HomePage;
---
+++ frontend/src/pages/UserPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export class UserPage extends React.Component {
+    render() {
+        return(
+            <div data-testid="userpage">
+
+            </div>
+        )
+    }
+}
+
+export default UserPage;
