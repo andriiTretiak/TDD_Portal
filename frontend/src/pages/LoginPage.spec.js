fd33c3e883cfc117e22735c8c38c27fa44d9b4f6
Andrey <CPdSCabellos@gmail.com>
2020-03-08 13:26:12 +0200

    (Front) Add redirect from LoginPage to HomePage after success login

--- frontend/src/pages/LoginPage.spec.js
+++ frontend/src/pages/LoginPage.spec.js
@@ -203,6 +203,20 @@ describe('Login page', () => {
             const spinner = queryByText('Loading...');
             expect(spinner).not.toBeInTheDocument();
         });
+        it('redirects to homepage after successful login', async () => {
+            const actions = {
+                postLogin: jest.fn().mockResolvedValue({})
+            };
+
+            const history ={
+                push: jest.fn()
+            };
+
+            setupForSubmit({actions, history});
+            fireEvent.click(button);
+            await waitForDomChange();
+            expect(history.push).toHaveBeenCalledWith('/');
+        });
     });
 });
 
