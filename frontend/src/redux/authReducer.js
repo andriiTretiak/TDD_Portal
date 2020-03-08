c2ce26c4374acb1a4d3e20ca1c8a4a5b57c29530
Andrey <CPdSCabellos@gmail.com>
2020-03-08 15:17:26 +0200

    (Front) Add redux

---
+++ frontend/src/redux/authReducer.js
@@ -0,0 +1,15 @@
+
+const initialState = () => {
+    return {
+        id:0,
+        username: '',
+        displayName: '',
+        image: '',
+        password: '',
+        isLoggedIn: false
+    }
+};
+
+export default function authReducer(state = initialState(), action) {
+    return state;
+}
