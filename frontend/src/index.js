3f7b52bf6da10cb357ed429a1e2b5dc7d7b71591
Andrey <CPdSCabellos@gmail.com>
2020-03-08 09:41:08 +0200

    (Front) Add LoginPage

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,14 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
-import {UserSignupPage} from "./pages/UserSignupPage";
+import {LoginPage} from "./pages/LoginPage";
 import * as apiCalls from "./api/apiCalls";
 
 const actions ={
     postSignup: apiCalls.signup
 };
 
-ReactDOM.render(<UserSignupPage actions={actions}/>, document.getElementById('root'));
+ReactDOM.render(<LoginPage/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
