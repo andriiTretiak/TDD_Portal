ed4249dff63e32c0631f5e3caac48d21e7c445ab
Andrey <CPdSCabellos@gmail.com>
2020-03-09 15:06:37 +0200

    (Front)Add state to localStorage

--- frontend/src/containers/App.spec.js
+++ frontend/src/containers/App.spec.js
@@ -7,6 +7,8 @@ import axios from 'axios';
 import App from './App';
 import configureStore from '../redux/configureStore';
 
+beforeEach(() => localStorage.clear());
+
 const setup = path => {
     const store = configureStore(false);
     return render(
@@ -133,5 +135,46 @@ describe('App', () => {
         fireEvent.click(button);
         const myProfileLink = await waitForElement(() => queryByText('Profile'));
         expect(myProfileLink).toBeInTheDocument();
-    })
+    });
+
+    it('saves logged in user data to localStorage after login success', async () => {
+        const { queryByPlaceholderText, container, queryByText } = setup('/login');
+        const userNameInput = queryByPlaceholderText('Your username');
+        const passwordInput = queryByPlaceholderText('Your password');
+        fireEvent.change(userNameInput, changeEvent('user1'));
+        fireEvent.change(passwordInput, changeEvent('P4ssword'));
+        const button = container.querySelector('button');
+        axios.post = jest.fn().mockResolvedValue({
+            data: {
+                id: 1,
+                username: 'user1',
+                displayName: 'display1',
+                image: 'profile1.png',
+            }
+        });
+        fireEvent.click(button);
+        await waitForElement(() => queryByText('Profile'));
+        const dataInStore = JSON.parse(localStorage.getItem('portal-auth'));
+        expect(dataInStore).toEqual({
+            id:1,
+            username: 'user1',
+            displayName: 'display1',
+            image: 'profile1.png',
+            password: 'P4ssword',
+            isLoggedIn: true
+        });
+    });
+    it('displays logged in tapBar when storage has logged in user data', () => {
+        localStorage.setItem('portal-auth', JSON.stringify({
+            id:1,
+            username: 'user1',
+            displayName: 'display1',
+            image: 'profile1.png',
+            password: 'P4ssword',
+            isLoggedIn: true
+        }));
+        const {queryByText} = setup('/');
+        const myProfileLink = queryByText('Profile');
+        expect(myProfileLink).toBeInTheDocument();
+    });
 });
