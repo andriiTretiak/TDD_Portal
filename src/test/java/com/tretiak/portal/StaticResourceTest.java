47e8e833a5f1b324d314c576b8727230a8161eb1
Andrey <CPdSCabellos@gmail.com>
2020-03-26 18:05:00 +0200

    (Back)Automate create uploads-test folder

---
+++ src/test/java/com/tretiak/portal/StaticResourceTest.java
@@ -0,0 +1,24 @@
+package com.tretiak.portal;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.io.File;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
+public class StaticResourceTest {
+    @Test
+    public void checkStaticFolder_whenAppIsInitialize_uploadFolderMustExist(){
+        File uploadFolder = new File("uploads-test");
+        boolean uploadFolderExists = uploadFolder.exists() && uploadFolder.isDirectory();
+        assertThat(uploadFolderExists).isTrue();
+    }
+}
