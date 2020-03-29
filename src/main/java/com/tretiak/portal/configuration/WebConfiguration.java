faef0754ccf98bc3aeb3ae588747d04f53b0cf38
Andrey <CPdSCabellos@gmail.com>
2020-03-29 12:03:15 +0300

    (Back)Add checks static resources

--- src/main/java/com/tretiak/portal/configuration/WebConfiguration.java
+++ src/main/java/com/tretiak/portal/configuration/WebConfiguration.java
@@ -3,6 +3,7 @@ package com.tretiak.portal.configuration;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.io.File;
@@ -14,6 +15,12 @@ public class WebConfiguration implements WebMvcConfigurer {
 
     public WebConfiguration(AppConfiguration appConfiguration) {
         this.appConfiguration = appConfiguration;
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/images/**")
+                .addResourceLocations("file:" + appConfiguration.getUploadPath() + "/");
     }
 
     @Bean
