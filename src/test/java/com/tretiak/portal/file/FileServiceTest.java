ad5e8a03b906f7e34b427cc573b786481f4548cb
Andrey <CPdSCabellos@gmail.com>
2020-03-30 19:13:57 +0300

    (Back)Detect fileType by Tika

---
+++ src/test/java/com/tretiak/portal/file/FileServiceTest.java
@@ -0,0 +1,50 @@
+package com.tretiak.portal.file;
+
+import com.tretiak.portal.configuration.AppConfiguration;
+import org.apache.commons.io.FileUtils;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.io.File;
+import java.io.IOException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@ActiveProfiles("test")
+public class FileServiceTest {
+
+    private FileService fileService;
+    private AppConfiguration appConfiguration;
+
+    @Before
+    public void init(){
+        appConfiguration = new AppConfiguration();
+        appConfiguration.setUploadPath("uploads-test");
+
+        fileService = new FileService(appConfiguration);
+
+        new File(appConfiguration.getUploadPath()).mkdir();
+        new File(appConfiguration.getFullProfileImagesPath()).mkdir();
+        new File(appConfiguration.getFullAttachmentsPath()).mkdir();
+    }
+
+    @Test
+    public void detectType_whenPngFileProvided_returnsImagePng() throws IOException {
+        ClassPathResource resourceFile = new ClassPathResource("test-png.png");
+        byte[] fileArr = FileUtils.readFileToByteArray(resourceFile.getFile());
+        String fileType = fileService.detectType(fileArr);
+        assertThat(fileType).isEqualTo("image/png");
+    }
+
+    @After
+    public void cleanup() throws IOException {
+        FileUtils.cleanDirectory(new File(appConfiguration.getFullAttachmentsPath()));
+        FileUtils.cleanDirectory(new File(appConfiguration.getFullProfileImagesPath()));
+    }
+}
